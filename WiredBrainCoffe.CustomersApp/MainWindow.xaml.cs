c625e6f3407a4965e22f2d7b8312ea15d39df119
LTWiener <Bastian.Frese@krause-systems.de>
2024-09-30 08:09:41 +0200

    Add files via upload

---
+++ WiredBrainCoffe.CustomersApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WiredBrainCoffe.CustomersApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+          var column = (int)CustomerListGrid.GetValue(Grid.ColumnProperty);
+
+            var newColumn = column == 0 ? 2 : 0;
+            CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+        }
+
+        private void ButtonZukunft_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
